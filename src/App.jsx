d647bd4ee53130e06ce85aa2704b2dd877637b1c
maestro194 <dongboy1904@gmail.com>
2023-12-11 13:15:31 +0700

    Update basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Search from './pages/Search'
 
 export default function App() {
-  return <h1 className="text-red-500">App</h1>
+  return <BrowserRouter>
+    <Routes>
+      <Route path='/sign-in' element={<Signin/>}/>
+      <Route path='/search' element={<Search/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/home' element={<Home/>}/>
+    </Routes>
+  </BrowserRouter>
 }
