8123fc8afb8b2c93d35f017db0e45daeaf6a31c2
maestro194 <dongboy1904@gmail.com>
2023-12-11 15:29:06 +0700

    Database connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("MongoDB connected")
+})
 
 const app = express();
 
