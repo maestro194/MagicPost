1de2913fbd743a7eaf29400c9b8d46502bf861e9
maestro194 <dongboy1904@gmail.com>
2023-12-13 09:01:59 +0700

    UI for signin/signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Signin from './pages/Signin'
+import Signup from './pages/Signup'
 import Search from './pages/Search'
 import Header from './components/Header'
 
@@ -11,7 +12,8 @@ export default function App() {
     <Header/>
 
     <Routes>
-      <Route path='/sign-in' element={<Signin/>}/>
+      <Route path='/signin' element={<Signin/>}/>
+      <Route path='/signup' element={<Signup/>}/>
       <Route path='/search' element={<Search/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/home' element={<Home/>}/>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Signin() {
+export default function Signup() {
   return (
     <div className='p-3 max-w-lg mx-auto'>
         <h1 className='text-3xl text-center font-bold my-8'>
-            Sign in
+            Sign up
         </h1>
 
         <form className='flex flex-col gap-6'>
@@ -14,14 +15,26 @@ export default function Signin() {
                 className='border p-3 rounded-lg'
                 id='username'/>
             <input 
+                type="email" 
+                placeholder='Email'
+                className='border p-3 rounded-lg'
+                id='email'/>
+            <input 
                 type="password" 
                 placeholder='Password'
                 className='border p-3 rounded-lg'
                 id='password'/>
             <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover: opacity-95 disabled:opacity-80'>
-                Sign in
+                Sign up
             </button>
         </form>
+
+        <div className='flex gap-2 mt-6'>
+            <p>Have an account?</p>
+            <Link to={'/signin'}>
+                <span className='text-blue-700'>Sign in</span>
+            </Link>
+        </div>
     </div>
   )
 }
