f9d0666a39282b73656e04ac3f5727517666cbb8
maestro194 <dongboy1904@gmail.com>
2023-12-21 18:10:14 +0700

    Basic UI for user

--- client/src/pages/GeneralManager/GM.jsx
+++ client/src/pages/GeneralManager/GM.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { useState } from 'react'
 import { FaHome, FaUser, FaCube } from 'react-icons/fa'
 
 export default function GM() {
-  const [formData, setFormData] = useState({});
+  const showProfile = () => {
+    console.log('clicked')
+  }
+
   return (
     <div>
       {/* left navi */}
@@ -12,7 +14,7 @@ export default function GM() {
           <ul className='gap-4'>
             <li 
               className='flex gap-3 w-full h-12 py-3 pl-4 hover:bg-slate-100'
-              onClick={() => console.log('clicked')}
+              onClick={showProfile}
             >
               <FaHome className='m-1'/>
               <span className='hidden md:inline-flex'>Home</span>
--- client/src/pages/Office/OfficeEmployee.jsx
+++ client/src/pages/Office/OfficeEmployee.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import { FaHome, FaUser, FaCube } from 'react-icons/fa'
 
 export default function OfficeEmployee() {
   return (
-    <div>OfficeEmployee</div>
+    <div>
+      {/* left navi */}
+      <div className='flex'>
+        <div className='bg-white w-16 md:w-48 h-screen overflow-hidden border'>
+          <ul className='gap-4'>
+            <li 
+              className='flex gap-3 w-full h-12 py-3 pl-4 hover:bg-slate-100'
+              onClick={() => console.log('clicked')}
+            >
+              <FaHome className='m-1'/>
+              <span className='hidden md:inline-flex'>Home</span>
+            </li>
+            <li 
+              className='flex gap-3 w-full h-12 py-3 pl-4 hover:bg-slate-100'
+              onClick={() => console.log('clicked')}
+            >
+              <FaUser className='m-1'/>
+              <span className='hidden md:inline-flex'>User</span>
+            </li>
+            <li 
+              className='flex gap-3 w-full h-12 py-3 pl-4 hover:bg-slate-100'
+              onClick={() => console.log('clicked')}
+            >
+              <FaCube className='m-1'/>
+              <span className='hidden md:inline-flex'>Package</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
