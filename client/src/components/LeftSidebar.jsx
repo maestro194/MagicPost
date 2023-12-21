007c7617999fd0f13c7c9cd067f64ef255133a3b
maestro194 <dongboy1904@gmail.com>
2023-12-21 11:14:36 +0700

    Minor detail fix

---
+++ client/src/components/LeftSidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LeftSidebar() {
+  return (
+    <div className='max-h-screen bg-slate-400 w-6 p-6'>
+      this
+    </div>
+  )
+}
--- client/src/pages/GeneralManager/GM.jsx
+++ client/src/pages/GeneralManager/GM.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
+import LeftSideBar from '../../components/LeftSidebar'
 
 export default function GM() {
   return (
     <div>
-      <button className='bg-slate-500 rounded-lg p-3 m-3'>
-        Create Account
-      </button>
-
-      <button className='bg-slate-500 rounded-lg p-3 m-3'>
-        Accounts
-      </button>
-
-      <button className='bg-slate-500 rounded-lg p-3 m-3'>
-        Packages
-      </button>
+      <LeftSideBar/>
     </div>
   )
 }
