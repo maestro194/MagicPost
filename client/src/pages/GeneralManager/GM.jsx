190e5d13deb8a65fae33edc5a73124661e502cfa
maestro194 <dongboy1904@gmail.com>
2023-12-23 18:17:02 +0700

    Update transaction, office and warehouse schema, have routing issues

--- client/src/pages/GeneralManager/GM.jsx
+++ client/src/pages/GeneralManager/GM.jsx
@@ -1,39 +1,42 @@
 import React from 'react'
 import { FaHome, FaUser, FaCube } from 'react-icons/fa'
 import ProfileCard from '../../components/ProfileCard'
+import { Link } from 'react-router-dom'
 
 export default function GM() {
-  const showProfile = () => {
-    console.log('clicked')
-  }
-
   return (
     <div>
       {/* left navi */}
       <div className='flex'>
         <div className='bg-white w-16 md:w-48 h-screen overflow-hidden border'>
           <ul className='gap-4'>
-            <li 
-              className='flex gap-3 w-full h-12 py-3 pl-4 hover:bg-slate-100'
-              onClick={showProfile}
-            >
-              <FaHome className='m-1'/>
-              <span className='hidden md:inline-flex'>Home</span>
-            </li>
-            <li 
-              className='flex gap-3 w-full h-12 py-3 pl-4 hover:bg-slate-100'
-              onClick={() => console.log('clicked')}
-            >
-              <FaUser className='m-1'/>
-              <span className='hidden md:inline-flex'>User</span>
-            </li>
-            <li 
-              className='flex gap-3 w-full h-12 py-3 pl-4 hover:bg-slate-100'
-              onClick={() => console.log('clicked')}
-            >
-              <FaCube className='m-1'/>
-              <span className='hidden md:inline-flex'>Package</span>
-            </li>
+            <Link to="/gm/home">
+              <li 
+                className='flex gap-3 w-full h-12 py-3 pl-4 hover:bg-slate-100'
+                // onClick={setSelected('home')}
+              >
+                <FaHome className='m-1'/>
+                <span className='hidden md:inline-flex'>Home</span>
+              </li>
+            </Link>
+            <Link to="/gm/users">
+              <li 
+                className='flex gap-3 w-full h-12 py-3 pl-4 hover:bg-slate-100'
+                // onClick={setSelected('user')}
+              >
+                <FaUser className='m-1'/>
+                <span className='hidden md:inline-flex'>User</span>
+              </li>
+            </Link>
+            <Link to="/gm/packages">
+              <li 
+                className='flex gap-3 w-full h-12 py-3 pl-4 hover:bg-slate-100'
+                // onClick={setSelected('package')}
+              >
+                <FaCube className='m-1'/>
+                <span className='hidden md:inline-flex'>Package</span>
+              </li>
+            </Link>
           </ul>
         </div>
 
